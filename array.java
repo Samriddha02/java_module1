e9e0ec604ab393c76f28112d0fe9a09200236bc9
Samriddha02 <somaghoshsurya@gmail.com>
2025-08-19 15:33:59 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args) {
+        int n[] = new int[5];
+        n[0]=5;
+        n[1]=2;
+        n[2]=3;
+        n[3]=4;
+        System.out.println(n[2]);
+    }
+}
